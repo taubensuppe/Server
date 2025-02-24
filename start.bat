0029a82133fb30486fb5ca29817373de621bb2e8
taubensuppe | GalaxyServices <86103687+taubensuppe@users.noreply.github.com>
2025-02-24 01:28:27 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Successfully Installed, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get a troop tunnel, make sure ngrok_auth_token is correct in Settings> Secrets> Repository Secret.Maybe your previous VM is still running:
-https://dashboard.ngrok.com/status/tunnels "
+echo https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
 echo Password: W2016
 echo Please log in to your RDP!
